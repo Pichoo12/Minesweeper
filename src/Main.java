79d2a4c307bc30c57ec555bee6e59190160a2d5c
Pichoo12 <s1802049@online.houstonisd.org>
2023-04-03 10:44:03 -0500

    init

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+
 public class Main extends JFrame{
-	private static final int WIDTH =1800;
-	private static final int HEIGHT=1600;
+	
+	private static JFrame frame;
+	private static String title;
+
+	private static final int WIDTH =700;
+	private static final int HEIGHT=700;
 	public Main () {
-		super("Space Invaders");
+		super("Mine Island");
 		setSize(WIDTH, HEIGHT);
 		Game play = new Game();
 		((Component) play).setFocusable(true);
@@ -18,4 +23,5 @@ public class Main extends JFrame{
 	public static void main(String[] args) {
 		Main run = new Main();
 	}
+
 }
