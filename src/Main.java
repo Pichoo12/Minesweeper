68ea60e3c4669025dbbd6eece5b8d9f6abde7b8f
Pichoo12 <s1802049@online.houstonisd.org>
2023-04-05 10:21:34 -0500

    INIT

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,20 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 
-public class Main extends JFrame{
-	
-	private static JFrame frame;
-	private static String title;
+//EXAMPLE FRAME NOT OURS 
 
-	private static final int WIDTH = 700;
-	private static final int HEIGHT= 700;
-	public Main () {
-		super("Mine Island");
-		setSize(WIDTH, HEIGHT);
-		Game play = new Game();
-		((Component) play).setFocusable(true);
-		Color RoyalBlue = new Color(50,200,200);
-		setBackground(RoyalBlue);
-		getContentPane().add(play);
-		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-	public static void main(String[] args) {
-		Main run = new Main();
-	}
 
+import javax.swing.JFrame;
+import java.awt.GridLayout;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MineSweeper");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(1,1)); // set layout of JFrame to a 1x1 grid
+        
+        Grid gridPanel = new Grid(new GridLayout(Game.grid, Game.grid)); // create new Grid panel
+        
+        frame.add(gridPanel); // add Grid panel to JFrame
+        frame.pack(); // resize JFrame to fit contents
+        frame.setVisible(true); // display JFrame
+    }
 }
