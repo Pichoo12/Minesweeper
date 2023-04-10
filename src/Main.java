e60cb22db4c1b82af0b2cd47e29b20ce520fa0f4
Pichoo12 <s1802049@online.houstonisd.org>
2023-04-10 11:18:46 -0500

    init

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ public class Main {
     public static void main(String[] args) {
     	
         JFrame frame = new JFrame();
+        Game play = new Game();
+
         
         //Formatting 
         //frame.setSize(Game.WIDTH, Game.HEIGHT);
@@ -20,12 +22,15 @@ public class Main {
         
         // positions Jframe in middle
         frame.setLocationRelativeTo(null);
+        frame.getContentPane().add(play);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         //assigned to variable grid 
         // found in game class
         Grid grid = new Grid(new GridLayout(Game.grid, Game.grid));
         Color RoyalBlue = new Color(173,216,230);
         grid.setBackground(RoyalBlue); 
+
         
         //DONT DELETE this is what adds grid object to frame 
        frame.add(grid);
